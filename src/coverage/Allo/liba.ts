970949eca4aca9c20a5c69ec44c6aafd45f25eb9
Trukhanov Kostya <ktrukhanov@intersog.com>
2024-05-02 18:42:19 +0300

    set some headers to req

--- src/coverage/Allo/liba.ts
+++ src/coverage/Allo/liba.ts
@@ -46,9 +46,12 @@ export const getProductPage: GetProductPage = async (url) => {
     return await axios.get(url, {
       maxRedirects: 0,
       headers: {
-        Accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'User-Agent': 'python-requests/2.26.0',
+        'Accept-Encoding': 'gzip, deflate',
+        Accept: '*/*',
+        Connection: 'keep-alive',
+        'cache-control': 'max-age=0',
+        'accept-language': 'en-US,en;q=0.9',
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       },
