bb9e5cddbcd4b9d3ec6d98b65fa15eb282766d0b
Trukhanov Kostya <ktrukhanov@intersog.com>
2024-05-02 18:52:42 +0300

    another headers

--- src/coverage/Allo/liba.ts
+++ src/coverage/Allo/liba.ts
@@ -46,13 +46,18 @@ export const getProductPage: GetProductPage = async (url) => {
     return await axios.get(url, {
       maxRedirects: 0,
       headers: {
-        'Accept-Encoding': 'gzip, deflate',
-        Accept: '*/*',
-        Connection: 'keep-alive',
+        authority: 'allo.ua',
         'cache-control': 'max-age=0',
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+        'sec-fetch-dest': 'document',
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
         'accept-language': 'en-US,en;q=0.9',
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
       },
     });
   } catch (err) {
