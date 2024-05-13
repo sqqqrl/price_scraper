47f2e692ba10d31600a5e5cb8e438a3f32e91d51
Trukhanov Kostya <ktrukhanov@intersog.com>
2024-05-13 16:36:55 +0300

    modify logger

--- src/coverage/Allo/categories/liba.ts
+++ src/coverage/Allo/categories/liba.ts
@@ -34,32 +34,36 @@ const scrapProducts = async (link: string): Promise<ProductDto[]> => {
 
   while (!isEnd) {
     // scrap products info from state
-    const { pagination, products } = (await defaultPage.evaluate(
-      `window.__ALLO__.state['catalog/category/product-list']`
-    )) as ProductList;
+    try {
+      const { pagination, products } = (await defaultPage.evaluate(
+        `window.__ALLO__.state['catalog/category/product-list']`
+      )) as ProductList;
 
-    result.push(
-      ...products.map((product) => ({
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        rating: product.rating,
-        url: product.url,
-      }))
-    );
+      result.push(
+        ...products.map((product) => ({
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          rating: product.rating,
+          url: product.url,
+        }))
+      );
 
-    if (
-      pagination.current_page ===
-      Math.ceil(pagination.total_number_of_items / pagination.items_per_page)
-    ) {
-      isEnd = true;
-      break;
+      if (
+        pagination.current_page ===
+        Math.ceil(pagination.total_number_of_items / pagination.items_per_page)
+      ) {
+        isEnd = true;
+        break;
+      }
+
+      await defaultPage.goto(`${link}/p-${pagination.current_page + 1}`, {
+        waitUntil: 'domcontentloaded',
+      });
+      await setTimeout(randTimeout(2000, 3500));
+    } catch (err) {
+      logger.error('error', new Error(`${err}`));
     }
-
-    await defaultPage.goto(`${link}/p-${pagination.current_page + 1}`, {
-      waitUntil: 'domcontentloaded',
-    });
-    await setTimeout(randTimeout(2000, 3500));
   }
 
   return result;
@@ -68,21 +72,22 @@ const scrapProducts = async (link: string): Promise<ProductDto[]> => {
 export const processLinks = async (links: string[]): Promise<void> => {
   for (const link of links) {
     try {
+      logger.error('error', new Error('rlerlw'));
       logger.log(
         'info',
-        `------------ ${links.indexOf(link) + 1} of ${links.length} category processing ------------`
+        `${links.indexOf(link) + 1} of ${links.length} category processing.`
       );
-      logger.log('info', `---- Starting scrap category link: ${link} ----`);
+      logger.log('info', `Starting scrap category link: ${link}`);
       const a = performance.now();
       const products = await scrapProducts(link);
       const b = performance.now();
-      logger.log('info', `time spent on scrap === ${b - a}`);
+      logger.log('info', `time spent on scrap: ${b - a}`);
       logger.log(
         'info',
-        `start saving products info in db. count: ${products.length}`
+        `Start saving products info in db. Count: ${products.length}`
       );
       await productServiceAllo.saveAll(products);
-      logger.log('info', 'complete saving');
+      logger.log('info', 'Complete saving');
     } catch (err) {
       logger.error('error', new Error(`${err}`));
     }
--- src/liba/logger.ts
+++ src/liba/logger.ts
@@ -1,14 +1,38 @@
-import { createLogger, transports, format } from 'winston';
+import { createLogger, transports, format, addColors } from 'winston';
 
-const { combine, errors, json, colorize, simple, prettyPrint } = format;
+const { combine, timestamp, json, simple, colorize, label, printf } = format;
+
+// const formatter = format((info) => {
+//   const { message, timestamp, label, level } = info;
+
+//   info.message = ` ${label} ${timestamp}  ${level} : ${message}`;
+//   delete info.timestamp;
+//   delete info.label;
+//   return info;
+// });
 
 export const logger = createLogger({
   transports: [new transports.Console()],
   format: combine(
-    colorize({ all: true }),
-    simple(),
+    label({
+      label: '[LOGGER]',
+    }),
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
     json(),
-    errors({ stack: true }),
-    prettyPrint()
+    simple(),
+    colorize({ all: true }),
+    printf(
+      (info) =>
+        ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`
+    )
   ),
+});
+
+addColors({
+  info: 'bold blue', // fontStyle color
+  warn: 'italic yellow',
+  error: 'bold red',
+  debug: 'green',
 });
